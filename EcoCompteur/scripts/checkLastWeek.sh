9ab1e24ab2c5255e0fd33cd2b6ac3bc79bd566ec
fabien <fabien.pelisson@innovisite.com>
2016-05-16 14:17:21 +0200

    Add instance checking

--- EcoCompteur/scripts/checkLastWeek.sh
+++ EcoCompteur/scripts/checkLastWeek.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+. /etc/BEV/scripts/BlueSystem/Logs.inc
+
 CLIENTBLUEPORTAIL=$1
 
-php /var/www/EcoCompteur/CheckLastDays.php $(date +%Y%m%d) 20 $CLIENTBLUEPORTAIL > /tmp/lastWeekConvertOutput
+DATE=$(date +%Y%m%d)
+HISTORY=2
+CMD=checkLastWeek
+
+if [ $(ps -e | grep -c $CMD) -gt 2 ]; then
+    Logs_add $0 "Instance already running cannot perform check for date $DATE and $HISTORY days"
+    exit -1;
+fi
+
+php /var/www/EcoCompteur/CheckLastDays.php $DATE $HISTORY $CLIENTBLUEPORTAIL > /tmp/lastWeekConvertOutput
 
